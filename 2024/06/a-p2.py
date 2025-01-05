ae0d2c108efae4ce7e04ac969b0bc37bdc99d13f
Han Le <hanle.cs23@gmail.com>
2025-01-05 01:35:41 -0500

    Update current progress for day 6 part 2

--- 2024/06/a-p2.py
+++ 2024/06/a-p2.py
@@ -1,5 +1,15 @@
 from aocd import get_data
 data = get_data(day=6, year=2024)
+data = '''....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...'''
 data = data.splitlines()
 HEIGHT = len(data)
 WIDTH = len(data[0])
@@ -10,6 +20,7 @@ possibleFacing = ["v", "<", "^", ">"]
 
 
 # Finding starting position and starting direction
+startingDirection = None
 currentDirectionIndex = None
 currentPosition = None
 for i in range(len(data)):
@@ -20,18 +31,28 @@ for i in range(len(data)):
             currentPosition = (i,j)
         if not currentDirectionIndex:
             currentDirectionIndex = possibleFacing.index(data[i][j])
+            startingDirection = possibleFacing.index(data[i][j])
 
 if currentPosition == None or currentDirectionIndex == None:
     print("Error: currentPositiona or currentDirection is Null")
 
-visited = set()
+def render(data, currentDirection, currentPosition):
+    renderData = data[:]
+    renderData[currentPosition[0]] = renderData[currentPosition[0]][:currentPosition[1]] + currentDirection +  renderData[currentPosition[0]][currentPosition[1] + 1:]
+    for i in range(len(data)):
+        print(renderData[i])
+    print("-" * len(data[0]))
+
+
+
+visited = []
+possiblePlacement = set()
 flag = True
 while flag:
     if currentPosition == None or currentDirectionIndex == None:
         flag = False
         continue
-    if currentPosition not in visited:
-        visited.add(currentPosition)
+    visited.append(currentPosition)
     nextPosition = (currentPosition[0] + directions[currentDirectionIndex][0],currentPosition[1] + directions[currentDirectionIndex][1])
     if nextPosition[0] < 0 or nextPosition[0] >= HEIGHT or nextPosition[1] < 0 or nextPosition[1] >= WIDTH:
         flag = False
@@ -41,7 +62,8 @@ while flag:
     else:
         currentPosition = nextPosition
 
-possiblePlacement = set()
+
 # All possible placement can be in any direction of any visited point
 # No need to try all possible location in the map
-print(len(visited))
+# Can constantly looking to the next direction to see if there will be a block in that direction
+# Need to skip one position after changing direction after start scanning
